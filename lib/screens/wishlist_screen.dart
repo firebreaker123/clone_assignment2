eb1ec6a9121ebab0d2baf41f787ab955d450c4d0
firebreaker123 <dwightbat26@gmail.com>
2026-07-16 10:53:15 +0900

    Add hearts snackbar and white word of product name under images

--- lib/screens/wishlist_screen.dart
+++ lib/screens/wishlist_screen.dart
@@ -24,9 +24,11 @@ class WishlistScreen extends StatelessWidget {
           child: Padding(
             padding: EdgeInsets.only(left: 16, top: 8),
             child: Text(
-              "4 items",
+              (wishlistProducts.length == 1)
+                  ? "${wishlistProducts.length} item"
+                  : "${wishlistProducts.length} items",
               style: TextStyle(
-                color: Color.fromARGB(255, 87, 87, 87),
+                color: Color.fromARGB(255, 124, 124, 124),
                 fontSize: 14,
               ),
             ),
@@ -37,22 +39,53 @@ class WishlistScreen extends StatelessWidget {
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2,
               crossAxisSpacing: 20,
-              mainAxisSpacing: 50,
-              childAspectRatio: 1,
+              mainAxisSpacing: 40,
+              childAspectRatio: 0.8,
             ),
             itemCount: wishlistProducts.length,
             itemBuilder: (context, index) {
-              return Stack(
+              return Column(
                 children: [
-                  Padding(
-                    padding: (index % 2 == 0)
-                        ? EdgeInsets.only(left: 20, top: 20)
-                        : EdgeInsets.only(right: 20, top: 20),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image.asset(
-                        wishlistProducts[index].imageName,
-                        fit: BoxFit.cover,
+                  Expanded(
+                    child: Stack(
+                      fit: StackFit.expand,
+                      children: [
+                        Padding(
+                          padding: (index % 2 == 0)
+                              ? EdgeInsets.only(left: 20, top: 20)
+                              : EdgeInsets.only(right: 20, top: 20),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.asset(
+                              wishlistProducts[index].imageName,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          //padding: (index % 2 == 0) ? EdgeInsets.only(right: 15, bottom: 140,) : EdgeInsets.only(right: 35, bottom: 140,),
+                          top: 40,
+                          right: (index % 2 == 0) ? 15 : 35,
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: HeartSnackBar(snackState: true),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: (index % 2 == 0)
+                          ? EdgeInsets.only(left: 20, top: 20)
+                          : EdgeInsets.only(top: 20,),
+                      child: Text(
+                        wishlistProducts[index].productName,
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          fontSize: 15,
+                        ),
                       ),
                     ),
                   ),
